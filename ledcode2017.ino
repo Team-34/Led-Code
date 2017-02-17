d57645520e396183efaaca509d723684287a3b26
Bernard Allotey <benard.allotey4@yahoo.com>
2017-02-17 10:10:23 -0600

    Comments

--- ledcode2017.ino
+++ ledcode2017.ino
@@ -2,42 +2,47 @@
 #include <FastLED.h>
 #include <Wire.h>
 
+//Here we define the Pin that the LEDs are plugged into
 #define DATA_PIN 2
-//#define CLK_PIN   4
+/*this is the LED type, ask Mr. Wiggles, if you are unsure,
+but it is almost always WS2812*/
 # define LED_TYPE WS2812
+//I would probably leave this as is
 # define COLOR_ORDER GRB
-# define NUM_LEDS 164
-# define SUB_LEDS 258
-#define BRIGHTNESS 99
+//The number of LEDs in the strip; Adrianna would happily count them for you
+# define NUM_LEDS 258
+//arbitrary brightness value between 0 and 255
+#define BRIGHTNESS 200
+//I dont think this does anything, but leave it there in case
 #define FRAMES_PER_SECOND 120
+//Defines the strip for the library;
 CRGB leds[NUM_LEDS];
-CRGB sub_leds[SUB_LEDS];
 
 //0 is for Blue, 1 is for Red
 int color = 0;
-int iter = 0;
-bool climb = false;
+bool isClimb = false;
 CRGB alliance;
 
 void setup() {
+	//clears the strip
 	FastLED.clearData();
 	FastLED.clear();
-	delay(3000);
-	//FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-	FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(sub_leds, SUB_LEDS).setCorrection(TypicalLEDStrip);
-	FastLED.setBrightness(BRIGHTNESS);
-	Serial.begin(9600);
-
+	delay(3000);//insanity delay
+	FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);//adds the leds to the library
+	FastLED.setBrightness(BRIGHTNESS);//sets brightness
+	Serial.begin(9600);//begin serial communication
+	//this may or may not do anything
 	if (Serial.available() > 0) {
 		lightCode(Serial.read());
 	}
 }
+//shhh, leave the next 4 here and dont do anything
 typedef void(*SimplePatternList[])();
 SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-
+//the demo code
 void demo() {
 	// Call the current pattern function once, updating the 'leds' array
 	gPatterns[gCurrentPatternNumber]();
@@ -53,7 +58,7 @@ void demo() {
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-
+//start of the demo code
 void nextPattern()
 {
 	// add one to the current pattern number, and wrap around at the end
@@ -114,7 +119,7 @@ void juggle() {
 		dothue += 32;
 	}
 }
-
+//end of the demo code
 void lightCode(char command) {
 	if (color == 0)
 	{
@@ -123,88 +128,97 @@ void lightCode(char command) {
 	else if (color == 1) {
 		alliance = CRGB::Red;
 	}
-
+	//command 0 and 1 set alliance color
 	if (command == '0') {
 		color = 1;
 		alliance = CRGB::Red;
-		for (int x = 0; x <= SUB_LEDS; x++) {
-			sub_leds[x] = CRGB::Red;
+		for (int x = 0; x <= NUM_LEDS; x++) {
+			leds[x] = CRGB::Red;
 		}
 		FastLED.show();
 	}
 	else if (command == '1') {
 		color = 0;
 		alliance = CRGB::Blue;
-		for (int x = 0; x <= SUB_LEDS; x++) {
-			sub_leds[x] = CRGB::Blue;
+		for (int x = 0; x <= NUM_LEDS; x++) {
+			leds[x] = CRGB::Blue;
 		}
 		FastLED.show();
 	}
+	//this runs the last 30 seconds code
 	else if (command == '2') {
 		if (color == 0) {
-			for (int x = 0; x <= SUB_LEDS; x++) {
-				sub_leds[x] = alliance;
+			for (int x = 0; x <= NUM_LEDS; x++) {
+				leds[x] = alliance;
 			}
 			FastLED.show();
 			delay(150);
 
-			for (int x = 0; x <= SUB_LEDS; x++) {
-				sub_leds[x] = CRGB::Black;
+			for (int x = 0; x <= NUM_LEDS; x++) {
+				leds[x] = CRGB::Black;
 			}
 			FastLED.show();
 			delay(150);
 		}
 	}
+	//climbing code
 	else if (command == '3') {
-		for (int x = 0; x <= SUB_LEDS; x++) {
-			sub_leds[x] = CRGB::Green;
+		for (int x = 0; x <= NUM_LEDS; x++) {
+			leds[x] = CRGB::Green;
 			FastLED.show();
 		}
 		while (true) {
-			for (int x = 0; x <= SUB_LEDS; x++) {
-				sub_leds[x] = CRGB::Green;
+			for (int x = 0; x <= NUM_LEDS; x++) {
+				leds[x] = CRGB::Green;
 			}
 			FastLED.show();
 			delay(150);
 
-			for (int x = 0; x <= SUB_LEDS; x++) {
-				sub_leds[x] = CRGB::Black;
+			for (int x = 0; x <= NUM_LEDS; x++) {
+				leds[x] = CRGB::Black;
 			}
 			FastLED.show();
 			delay(150);
 		}
 	}
+	//not aligned with goal
 	else if (command == '4') {
-		for (int i = 0; i <= SUB_LEDS; i++)
+		for (int i = 0; i <= NUM_LEDS; i++)
 		{
-			sub_leds[i] = CRGB::Red;
+			leds[i] = CRGB::Red;
 		}
 		FastLED.show();
 		delay(500);
-		for (int i = 0; i <= SUB_LEDS; i++) {
-			sub_leds[i] = CRGB::Black;
+		for (int i = 0; i <= NUM_LEDS; i++) {
+			leds[i] = CRGB::Black;
 		}
 		delay(500);
 	}
+	//somewhat aligned
 	else if (command == '5') {
-		for (int i = 0; i <= SUB_LEDS; i++) {
-			sub_leds[i] = CRGB::Orange;
+		for (int i = 0; i <= NUM_LEDS; i++) {
+			leds[i] = CRGB::Orange;
 		}
 		delay(250);
-		for (int i = 0; i <= SUB_LEDS; i++) {
-			sub_leds[i] = CRGB::Black;
+		for (int i = 0; i <= NUM_LEDS; i++) {
+			leds[i] = CRGB::Black;
 		}
 		delay(250);
 	}
+	//very aligned
 	else if (command == '6') {
-		for (int i = 0; i <= SUB_LEDS; i++) {
-			sub_leds[i] = CRGB::Green;
+		for (int i = 0; i <= NUM_LEDS; i++) {
+			leds[i] = CRGB::Green;
 			FastLED.show();
 		}
-		for (int i = 0; i <= SUB_LEDS; i++) {
-			sub_leds[i] = CRGB::Black;
+		for (int i = 0; i <= NUM_LEDS; i++) {
+			leds[i] = CRGB::Black;
 		}
 		delay(100);
+	}
+	//run the demo reel
+	else if (command == '7') {
+		demo();
 	}
 }
 
